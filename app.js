1be91a8b8f03167e1909190d47a5e7a35a3e2cc5
Avisakula123 <rohithreddyas000@gmail.com>
2020-04-10 16:50:53 -0500

    done

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ const app = express();
 //Mangoose connection
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
-const mongoDB = 'mongodb+srv://s538294:@Rohith123@cluster0-n9qxh.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = 'mongodb+srv://s538097:Mohan@321@cluster0-gpwcm.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser: true,useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));

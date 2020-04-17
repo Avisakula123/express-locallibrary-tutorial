1d303a69c83c15e5e47308ebf1b8813ad8fc2b85
Avisakula123 <rohithreddyas000@gmail.com>
2020-04-17 15:59:48 -0500

    commit

--- controllers/authorcontroller.js
+++ controllers/authorcontroller.js
@@ -2,40 +2,40 @@ const Author = require('../models/author');
 
 // Display list of all Authors.
 exports.author_list = function(req, res) {
-    res.send('Rajashekar Gande');
+    res.send('Rohith Reddy');
 };
 
 // Display detail page for a specific Author.
 exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+    res.send('DONE: Author detail: ' + req.params.id);
 };
 
 // Display Author create form on GET.
 exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.send('DONE: Author create GET');
 };
 
 // Handle Author create on POST.
 exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
+    res.send('DONE: Author create POST');
 };
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete GET');
+    res.send('DONE: Author delete GET');
 };
 
 // Handle Author delete on POST.
 exports.author_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete POST');
+    res.send('DONE: Author delete POST');
 };
 
 // Display Author update form on GET.
 exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+    res.send('DONE: Author update GET');
 };
 
 // Handle Author update on POST.
 exports.author_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update POST');
+    res.send('DONE: Author update POST');
 };
